50dbfe240215f129cfd655332471f36d9be7d6f7
ANDUS25 <anandchavan113@gmail.com>
2021-07-07 23:10:41 +0530

    Experiment Number 10

---
+++ Javascript/JS-exp-10.js
@@ -0,0 +1,20 @@
+// integer
+var Number=12;
+console.log(Number);
+
+// string
+var STR='Collections of character is know as "string"'
+console.log(STR);
+
+// Many variable in one line 
+var a=20, b=20, d=30, e=40
+var c= a+b*d-e+a
+console.log(c);
+
+// This variable is undefined
+var aa
+console.log(aa);
+
+// null means nothing
+var ab=null
+console.log(ab);
