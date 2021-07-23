b894ac1507a804e74a813f5c905058adbc41a6f0
ANDUS25 <anandchavan113@gmail.com>
2021-07-23 21:42:08 +0530

    js

---
+++ Javascript/js-13_14.js
@@ -0,0 +1,32 @@
+var a=100
+var b=50
+var c=350
+var d=21
+var e=103
+var f=2
+
+// addition
+console.log("addition is = " +""+ (a+b+c));
+
+// subtraction
+console.log("subtraction is = " +""+ (a-b-c+c));
+
+// division
+console.log("division is = "+""+ (a/b));
+
+// multiplication
+console.log("Multiplication is = "+""+ (a*b));
+
+//Exponentiation
+console.log("Exponential is = "+""+(b**f-(a*c)));
+
+// reminder
+console.log("reminder is = "+""+(e%d));
+
+// Incriment
+console.log("Before incriment = "+""+(f++));
+console.log("After incriment = "+""+ (f));
+
+// Decrement
+console.log("Before Decrement = "+""+(e--));
+console.log("After decrement = "+""+(e));
