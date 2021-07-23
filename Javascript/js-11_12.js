2952d7aa13ec5ef84ab6b76f64f799c7129e4b4c
ANDUS25 <anandchavan113@gmail.com>
2021-07-23 22:09:22 +0530

    js-13_14

--- Javascript/js-11_12.js
+++ Javascript/js-11_12.js
@@ -1,4 +1,20 @@
 var FrontEnd=["HTML", 'CSS', 'Javascript']
 console.log(FrontEnd);
 console.log(FrontEnd[1]);
-console.log("" + FrontEnd[0]+"' , '"+Fro);
+console.log("" + FrontEnd[0]+"' , '" + FrontEnd[1]+"' , '" +FrontEnd[2]+"' "+ "usign this is css simple web page of the website");
+
+var MERN=["MongoDB", 'Express', 'React', 'NodeJS']
+console.log(MERN + "This part is remeaning");
+
+var EngBranch=["Mechanial", 'Civil', 'Computer']
+
+// at ending
+console.log(EngBranch.push('IT'));
+console.log(EngBranch.push('ENTC'));
+console.log(EngBranch.push('Electrical'));
+console.log(EngBranch);
+
+// at starting
+console.log(EngBranch.unshift('Electronics'));
+console.log(EngBranch.unshift('Chemical'));
+console.log(EngBranch);
