009e0cf83056d43915a3aa16b19f78a17263c9a9
ANDUS25 <anandchavan113@gmail.com>
2021-07-26 15:02:24 +0530

    js-exp-15

---
+++ Javascript/JS_exp-15.js
@@ -0,0 +1,26 @@
+var a=200
+var b=100
+var c=2
+var d=3
+
+console.log("Addtion vaule is = " +""+ (a+b))
+
+console.log("Subtraction vaule is = " +""+ (a-b))
+
+console.log("Multilication vaule is = " +""+ (a*b))
+
+console.log("Exponential value is = " +""+ (c**d))
+
+console.log("division value is = " +""+ (a/c))
+
+console.log("Modulus value is = " +""+ (a%d))
+
+console.log("before Incriment value is = " + a++)
+console.log("after Incriment value is = " + a)
+
+console.log("before decrement value is = " + b--)
+console.log("after decrement value is = " + b)
+
+console.log("Is 'a' is greater than 'b' ? = " + (a>b))
+
+console.log("Is 'a' is less than 'b' ? = " + (a<b))
