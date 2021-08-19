d27760dbb8953e3e42634e031347c79a5e8b6cd5
ANDUS25 <anandchavan113@gmail.com>
2021-08-19 20:47:06 +0530

    exp-8-9-10

---
+++ advance_js/ad_js_exp_8-9-10.js
@@ -0,0 +1,19 @@
+var My_name= "abc"
+var My_name= "XYZ"
+
+console.log(My_name);
+
+let my_pincode = 4110
+let my_pincodes = 4110
+
+console.log(my_pincode);
+
+
+//  get error 
+// const abc
+// abc= zyx
+// console.log(abc);
+
+
+const abc = "zyx"
+console.log(abc);
