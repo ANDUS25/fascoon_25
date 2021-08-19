589adad812887f233b8350927589f558e88eedf7
ANDUS25 <anandchavan113@gmail.com>
2021-08-19 21:49:52 +0530

    exp-12-13

---
+++ advance_js/ad_js_exp-12-13.js
@@ -0,0 +1,15 @@
+class engineering{
+
+    branch(){
+        console.log("we have too many branche's");
+    }
+
+    ig(){
+        console.log("Mechanical, Civil, E&TC, IT, Computer");
+    }
+}
+
+engi= new engineering();
+
+engi.branch();
+engi.ig()
