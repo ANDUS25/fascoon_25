c64ccf12a50cc659172604b01f9434d998315cf5
ANDUS25 <anandchavan113@gmail.com>
2021-08-19 21:49:36 +0530

    exp-11

---
+++ advance_js/ad_js_exp_11.js
@@ -0,0 +1,26 @@
+// in "VAR" we can redecleared with same name of variable or reassigned it.
+
+var name= "ANDY!"
+name ="ANDY!!"
+var name= "ANDY!!!"
+
+console.log(name);
+
+// in "LET" we can just reasigned it but can't redecleared it.
+let country= "INDIA"
+country= "INDIA"
+// let country= "INDIA"
+
+console.log(country);
+
+
+// In "const" we can't redecleared it or reassigned it and also it must be to decleard with a same scoope
+const state= "Maharashtra"
+// state = "maharashtra"
+// const state= "Maharashtra"
+
+
+// const state
+// state= "punjab"
+
+console.log(state);
