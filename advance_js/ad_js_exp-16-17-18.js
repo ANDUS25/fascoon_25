6572f9b2fd12877f5818d494f3d82655e2c069c3
ANDUS25 <anandchavan113@gmail.com>
2021-09-02 18:25:39 +0530

    exp-16-17-18

---
+++ advance_js/ad_js_exp-16-17-18.js
@@ -0,0 +1,13 @@
+class Info{
+    constructor(){
+        this.Fname="Virat";
+        this.lname="Kohli";
+    }
+    captain(){
+        console.log("Now a days team india's captain is " + "'" + this.Fname +" "+ this.lname+"'") ;
+    }
+}
+
+
+v1 = new Info();
+v1.captain();
